a49db50617ead731b4bd0bc6a67c38563af97238
Jaysmart <ogunbunmijoshua60@gmail.com>
2026-01-14 17:47:50 +0100

    MedTrackBackend

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express()
+require('dotenv').config()
+const connect  = require("./database/db.connect")
+const userrouter = require('./route/user.route')
+const cors = require("cors")
+const ejs = require("ejs")
+
+currentUser = ""
+
+app.use(cors({origin:"*"}))
+app.use(express.json({limit:"50mb"}))
+app.use("/", userrouter)
+app.set("view engine", "ejs")
+
+const port = 8008
+
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`)
+})
---
+++ middleware/user.validation.js
@@ -0,0 +1,11 @@
+const yup = require("yup")
+
+const usernameRegex = /^[a-zA-Z0-9_.]{3,16}$/
+
+const validationSchema =  yup.object({
+    username:yup.string().min(3, "Username can not be less than three characters").matches(usernameRegex, "username must be unique").required("Username  is required"),
+    password:yup.string().trim().min(3, "password cannot be less than three characters").required("password is required"),
+    email:yup.string().trim().email("Must be a valid email address").required("email is required"),
+})
+
+module.exports = validationSchema
