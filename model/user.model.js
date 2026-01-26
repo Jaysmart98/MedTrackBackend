80a0e45200f5cde6d78654bd599181d8b569aa47
Jaysmart <ogunbunmijoshua60@gmail.com>
2026-01-26 16:46:45 +0100

    Format user model properties and ensure newline at end of file

--- model/user.model.js
+++ model/user.model.js
@@ -5,10 +5,10 @@ const userSchema = mongoose.Schema({
     username: { type: String, required : true},
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    profilePicture:{type:String},
-    verified:{type:Boolean, default:false}
+    profilePicture: { type: String },
+    verified: { type: Boolean, default: false }
 })
 const userModel = mongoose.model("MedTrack", userSchema);
 
 
-module.exports = {userModel}
+module.exports = { userModel }
