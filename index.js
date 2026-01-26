b1bee5fdda79f8dc2a771c72009cb02a85e1569a
Jaysmart <ogunbunmijoshua60@gmail.com>
2026-01-26 17:32:25 +0100

    Refactor CORS configuration to allow specific origin and methods in index.js

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require("express")
 const app = express()
+const cors = require("cors")
 const mongoose = require("mongoose")
 require('dotenv').config()
 const connect  = require("./database/db.connect")
 const userrouter = require('./route/user.route')
-const cors = require("cors")
 const ejs = require("ejs")
 
 currentUser = ""
 
-app.use(cors({origin:"*"}))
+app.use(cors({
+    origin: "http://localhost:5173", // Allow your React app
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true
+}));
+
 app.use(express.json({limit:"50mb"}))
 app.use("/", userrouter)
 app.set("view engine", "ejs")
